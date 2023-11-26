c4d302007d82fd6f709b82b0f20d3167b1426f0e
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-11-26 00:37:21 -0300

    refactor: composition pattern in input

---
+++ src/components/Input.tsx
@@ -0,0 +1,33 @@
+import { Search } from "lucide-react"
+import React, { ComponentProps } from "react"
+
+export const Input = {
+  Root: InputRoot,
+  Control: InputControl,
+  Prefix: InputPrefix,
+}
+
+type InputPrefixProps = ComponentProps<"div">
+function InputPrefix(props: InputPrefixProps) {
+  return <div {...props}></div>
+}
+
+type InputControlProps = ComponentProps<"input">
+function InputControl(props: InputControlProps) {
+  return (
+    <input
+      className="min-w-0 flex-1 bg-transparent text-zinc-900 placeholder-zinc-600 outline-none"
+      placeholder="Search"
+      {...props}
+    />
+  )
+}
+
+type InputRootProps = ComponentProps<"div">
+function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      className=" mx-1 flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}></div>
+  )
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -15,6 +15,7 @@ import PrimaryNavItem from "./PrimaryNavItem"
 import SecondaryNavItem from "./SecondaryNavItem"
 import UsedSpaceWidget from "./UsedSpaceWidget"
 import Profile from "./Profile"
+import { Input } from "../Input"
 
 type Props = {}
 
@@ -23,13 +24,12 @@ export default function Sidebar({}: Props) {
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className=" mx-1 flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search size={24} className="h-5 w-5 text-zinc-500" />
-        <input
-          className="min-w-0 flex-1 bg-transparent text-zinc-900 placeholder-zinc-600 outline-none"
-          placeholder="Search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control />
+      </Input.Root>
 
       <nav className="flex flex-col gap-1">
         <PrimaryNavItem icon={Home} title="Home" />
