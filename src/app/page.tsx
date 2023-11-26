8dbc4a03d81ad0204740dae38c6d1391d77cf3b5
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-11-26 02:57:01 -0300

    feat: settings nav tabs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SettingsTabs } from "@/components/SettingsTabs"
+import * as Tabs from "@radix-ui/react-tabs"
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingsTabs />
+    </>
+  )
 }
